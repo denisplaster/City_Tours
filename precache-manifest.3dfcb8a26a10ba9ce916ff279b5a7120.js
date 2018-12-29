6790d8483381997d2a25fd5281cd02096d1adcf3
DENIS PLASTER <denjeo@gmail.com>
2018-12-28 23:23:31 -0600

    Updates

---
+++ precache-manifest.3dfcb8a26a10ba9ce916ff279b5a7120.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7946545f2d951dc00d11",
+    "url": "/City_Tours/static/css/main.2a76727d.chunk.css"
+  },
+  {
+    "revision": "7946545f2d951dc00d11",
+    "url": "/City_Tours/static/js/main.7946545f.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/City_Tours/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "d3276db7f48462dbf879",
+    "url": "/City_Tours/static/js/runtime~main.d3276db7.js"
+  },
+  {
+    "revision": "b3df3db4fbe311b2be781a65d7050190",
+    "url": "/City_Tours/index.html"
+  }
+];
